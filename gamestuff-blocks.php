f22a185237c90a01ac84b39ff1b765a107a01564
nymphadora07 <cocnymphadora@gmail.com>
2026-07-26 06:57:16 +0700

    chore(release): bump version to 1.2.0

--- gamestuff-blocks.php
+++ gamestuff-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name:       GameStuff Blocks
  * Plugin URI:        https://gamestuff.id
  * Description:       Custom Gutenberg Blocks for GameStuff.id
- * Version:           1.0.0
+ * Version:           1.2.0
  * Requires at least: 6.9
  * Requires PHP:      8.0
  * Author:            Irvan Noerfazri
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @since 1.0.0
  */
-define( 'GAMESTUFF_BLOCKS_VERSION', '1.0.0' );
+define( 'GAMESTUFF_BLOCKS_VERSION', '1.2.0' );
 define( 'GAMESTUFF_BLOCKS_FILE', __FILE__ );
 define( 'GAMESTUFF_BLOCKS_PATH', plugin_dir_path( __FILE__ ) );
 define( 'GAMESTUFF_BLOCKS_URL', plugin_dir_url( __FILE__ ) );
