34e159b0e8ab1728d4f2ac65c09c7160844f007e
nymphadora07 <cocnymphadora@gmail.com>
2026-07-30 15:39:44 +0700

    refactor(content-scroll): remove ContentScroll bootstrap, no longer needed

--- includes/Core/Plugin.php
+++ includes/Core/Plugin.php
@@ -176,7 +176,6 @@ final class Plugin {
 
 		\GameStuff\Blocks\Toc\Toc::boot();
 		\GameStuff\Blocks\Accordion\Accordion::boot();
-		\GameStuff\Blocks\ContentScroll\ContentScroll::boot();
 		\GameStuff\Blocks\Infobox\Infobox::boot();
 
 		if ( is_admin() ) {
