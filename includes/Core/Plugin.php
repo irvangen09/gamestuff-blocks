de7bef8db64bd7d8e0b9596cfa6e070fff960e4e
nymphadora07 <cocnymphadora@gmail.com>
2026-07-31 16:03:46 +0700

    feat(info-list): wire InfoList::boot() into register_services()

--- includes/Core/Plugin.php
+++ includes/Core/Plugin.php
@@ -176,6 +176,7 @@ final class Plugin {
 		\GameStuff\Blocks\Toc\Toc::boot();
 		\GameStuff\Blocks\Accordion\Accordion::boot();
 		\GameStuff\Blocks\Infobox\Infobox::boot();
+		\GameStuff\Blocks\InfoList\InfoList::boot();
 
 		if ( is_admin() ) {
 			\GameStuff\Settings\SettingsPage::boot();
