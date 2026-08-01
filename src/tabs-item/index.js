856d001157da509f1038206cdad0a5c056ebb897
nymphadora07 <cocnymphadora@gmail.com>
2026-08-01 14:52:40 +0700

    chore: sync package version to 1.9.0

--- src/tabs-item/index.js
+++ src/tabs-item/index.js
@@ -1,8 +1,5 @@
 import { registerBlockType } from '@wordpress/blocks';
 
-import './style.scss';
-import './editor.scss';
-
 import Edit from './edit';
 import save from './save';
 import metadata from './block.json';
