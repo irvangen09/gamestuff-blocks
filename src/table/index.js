a88d31caa90f40b45ecd16f44e7d3cc80e9a6672
nymphadora07 <cocnymphadora@gmail.com>
2026-08-15 17:24:03 +0700

    fix(table): wire deprecated v1 into registerBlockType

--- src/table/index.js
+++ src/table/index.js
@@ -7,8 +7,10 @@ import metadata from './block.json';
 
 import Edit from './edit';
 import Save from './save';
+import deprecated from './deprecated';
 
 registerBlockType( metadata, {
 	edit: Edit,
 	save: Save,
+	deprecated,
 } );
