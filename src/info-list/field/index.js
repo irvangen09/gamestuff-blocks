2630cdfe1aa67b9678828e0cb5b771cdffcb6a39
nymphadora07 <cocnymphadora@gmail.com>
2026-09-15 10:50:01 +0700

    feat(info-list): wire deprecated entry for Field block

--- src/info-list/field/index.js
+++ src/info-list/field/index.js
@@ -4,8 +4,10 @@ import metadata from './block.json';
 
 import Edit from './edit';
 import Save from './save';
+import deprecated from './deprecated';
 
 registerBlockType( metadata, {
 	edit: Edit,
 	save: Save,
+	deprecated,
 } );
